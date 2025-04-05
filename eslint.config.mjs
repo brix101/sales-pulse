74753f06c16406640551e0e5a171d118e135f95c
Brixter Porras <brixterporras@gmail.com>
2025-04-05 17:39:26 +0800

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
-import js from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+/// <reference types="./types.d.ts" />
+
+import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { ignores: ['node_modules', 'dist'] },
+  { ignores: ["node_modules", "dist"] },
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    plugins: { tseslint },
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: {
+      tseslint,
+      import: importPlugin, // ✅ register the plugin here
+    },
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
@@ -18,27 +24,26 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
-      ...turboPlugin.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
-      '@typescript-eslint/consistent-type-imports': [
-        'warn',
-        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "separate-type-imports" },
       ],
-      '@typescript-eslint/no-misused-promises': [
+      "@typescript-eslint/no-misused-promises": [
         2,
         { checksVoidReturn: { attributes: false } },
       ],
-      '@typescript-eslint/no-unnecessary-condition': [
-        'error',
+      "@typescript-eslint/no-unnecessary-condition": [
+        "error",
         {
           allowConstantLoopConditions: true,
         },
       ],
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
     },
   },
 ]);
