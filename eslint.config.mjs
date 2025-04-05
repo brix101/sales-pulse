78982a0eb7031a37714088486c08dde0a8f03317
Brixter Porras <brixterporras@gmail.com>
2025-04-05 11:53:27 +0800

    refactor: Enhance ESLint configuration with TypeScript support and additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,44 @@
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { ignores: ["node_modules", "dist"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ['node_modules', 'dist'] },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { tseslint },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      ...turboPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        2,
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/no-unnecessary-condition': [
+        'error',
+        {
+          allowConstantLoopConditions: true,
+        },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    },
   },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  tseslint.configs.recommended,
 ]);
