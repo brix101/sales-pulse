27152c17f64266626d43e1e0e34f2a48c7c66e4a
Brixter Porras <brixterporras@gmail.com>
2025-04-05 11:23:59 +0800

    feat: Add ESLint and Prettier configuration with linting scripts

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { ignores: ["node_modules", "dist"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+]);
